3c15d1106da8ff89e335fad55f9d2b1c081c5f2b
Anastasia <shvayka92@mail.ru>
2022-06-13 14:22:35 +0300

    done hw cycles

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//задание 1
+        int case1 = 0;
+        while (case1 < 10) {
+            case1++;
+            System.out.print(case1 + " ");
+        }
+        System.out.println();
+
+        for (int i = 10; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+//задание 2
+        int friday = 1;
+        while (friday < 31) {
+            friday += 7;
+            if (friday < 31) {
+                System.out.println("Сегодня пятница, " + friday + "-е число. Пора готовить отчет");
+            }
+        }
+//задание 3
+        int yearStarting = 0;
+        for (; yearStarting <=2122; yearStarting += 79) {
+            if (yearStarting >=1822) {
+                System.out.println(yearStarting);
+            }
+
+        }
     }
 }
